04b0f2b67459025aaf43aa8edf54ec622f3f5f51
kishlay <kishlay141@gmail.com>
2025-03-27 09:53:17 +0530

    removed the gantt chart folder

--- BASIC_GANTT_CHART_BUILDER/scheduling_algorithms.py
+++
@@ -1,9 +0,0 @@
-def fcfs_scheduling(burst_times):
-    start_times = [0] * len(burst_times)
-    completion_times = [0] * len(burst_times)
-    
-    for i in range(1, len(burst_times)):
-        start_times[i] = start_times[i - 1] + burst_times[i - 1]
-        completion_times[i] = start_times[i] + burst_times[i]
-    
-    return start_times, completion_times
