9172c23a45d72758125f860ca7c41c53aa74e895
kishlay <kishlay141@gmail.com>
2025-03-28 02:17:09 +0530

    Removed PROCESS_MONITORING files and updated README

--- PROCESS_MONITORING/web_version/frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// filepath: c:\Users\Kishlay\OneDrive\Desktop\OS\PROCESS_MONITORING\web_version\frontend\tailwind.config.js
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
